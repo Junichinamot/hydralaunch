597407a50ff6cf95cac34fec4f23570dc09cfdf7
Junichinamot <zoltangera1@hotmail.com>
2022-04-23 23:38:06 +0100

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -8,7 +8,7 @@ const network = NETWORK.eth;
 
 // General metadata for Ethereum
 const namePrefix = "Hydra Bulls Club";
-const description = "Remember to replace this description";
+const description = "Hydra OGs - The first NFT Collection on $Hydra";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
@@ -52,8 +52,8 @@ const CHAIN = 'rinkeby'; // only rinkeby or polygon
 const CONTRACT_NAME = 'Hydra Bulls Club';
 const CONTRACT_SYMBOL = 'HBC';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0x519564aA0d19977E3F6e14147122608E07d6cc13';
+const TREASURY_ADDRESS = '0x8824B75FE8CC76B0E7fE728831DA09338dD736a7';
 const MAX_SUPPLY = 2222; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.0001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 25; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
